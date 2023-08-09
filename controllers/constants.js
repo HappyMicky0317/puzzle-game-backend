7282b88c3442be7b32b9281f74919d7db09126b4
happydream0317 <onedev975@gmail.com>
2023-08-09 05:33:23 +0300

    pretty code

--- controllers/constants.js
+++ controllers/constants.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const OPENAI_KEY = process.env.OPENAI_KEY;
 
 module.exports = {
-    OPENAI_KEY: OPENAI_KEY
+  OPENAI_KEY: OPENAI_KEY,
 };
--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: 'localhost', // Replace with your database host
-  port: '3306', // Replace with your database host
-  user: 'root', // Replace with your database username
-  password: '', // Replace with your database password
-  database: 'puzzle_game' // Replace with your database name
+  host: "localhost", // Replace with your database host
+  port: "3306", // Replace with your database host
+  user: "root", // Replace with your database username
+  password: "", // Replace with your database password
+  database: "puzzle_game", // Replace with your database name
 });
 
 connection.connect((error) => {
-    if (error) {
-      console.error('Error connecting to the database: ', error);
-    } else {
-      console.log('Connected to the database!');
-    }
+  if (error) {
+    console.error("Error connecting to the database: ", error);
+  } else {
+    console.log("Connected to the database!");
+  }
 });
 
 module.exports = connection;
