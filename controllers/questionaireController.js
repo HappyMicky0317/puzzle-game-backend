35e2bedf2929c025725e8734391a6fa66bb0fc10
happydream0317 <onedev975@gmail.com>
2023-09-24 13:13:55 +0000

    update

--- controllers/questionaireController.js
+++ controllers/questionaireController.js
@@ -81,7 +81,7 @@ const asking = async (req, res) => {
         }
       }      
     })
-    console.log(answer.answer)
+    console.log("answer.answer")
     res.status(200).json(answer.answer);
   } else {
     res.status(500).json(answer.message);
